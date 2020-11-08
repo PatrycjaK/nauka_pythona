8aedc28472fb79f15e12bc59d836ad246db751a7
PatrycjaK <PatrycjaK@users.noreply.github.com>
2020-11-08 15:10:26 +0100

    Prosty kalkulator-dodatkowe obliczenia

--- prog4.py
+++ prog4.py
@@ -7,9 +7,15 @@ def calculator(a, b, op):
         return a * b
     elif op == '/':
         return a / b
+    return 0
+
 
 if __name__ == '__main__':
     c = calculator(1, 2, '+')
     print(c)
     d = calculator(100, 20, '-')
     print(d)
+    e = calculator(5, 4, '*')
+    print(e)
+    f = calculator(100, 20, '/')
+    print(f)
