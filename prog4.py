5f0dbd606bde7666ba6bb0f6e8eac5466c10f04b
PatrycjaK <PatrycjaK@users.noreply.github.com>
2020-11-08 15:01:13 +0100

    Prosty kalkulator

---
+++ prog4.py
@@ -0,0 +1,15 @@
+def calculator(a, b, op):
+    if op == '+':
+        return a + b
+    elif op == '-':
+        return a - b
+    elif op == '*':
+        return a * b
+    elif op == '/':
+        return a / b
+
+if __name__ == '__main__':
+    c = calculator(1, 2, '+')
+    print(c)
+    d = calculator(100, 20, '-')
+    print(d)
