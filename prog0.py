eb0e79714fa31aa7c47ac93102028bdd1f3818c4
PatrycjaK <PatrycjaK@users.noreply.github.com>
2020-11-08 13:12:12 +0100

    Zadanie 12 z dodatkowymi 1,2,3 z poprawkami

--- prog0.py
+++ prog0.py
@@ -1,7 +1,7 @@
-# produkty = ["mleko", "bulka", "mieso", "sok", "arbuz", "szafa"]
-# ceny = [4, 1, 20, 5, 6, 505]
-produkty = ["mleko"]
-ceny = [4]
+produkty = ["mleko", "bulka", "mieso", "sok", "arbuz", "szafa"]
+ceny = [4, 1, 20, 5, 6, 505]
+# produkty = ["mleko"]
+# ceny = [4]
 
 produkt_cena = dict(zip(produkty, ceny))
 
@@ -11,7 +11,6 @@ def paragon(koszyk):
 
     if ilosc_prod_w_koszyku > 3 and do_zaplaty < 500:
         do_zaplaty = round((do_zaplaty - min(produkt_cena.values()))*0.95,2)
-
 
     if do_zaplaty >= 500 and ilosc_prod_w_koszyku >= 2:
         do_zaplaty = round((do_zaplaty - min(produkt_cena.values()))*0.90,2)
